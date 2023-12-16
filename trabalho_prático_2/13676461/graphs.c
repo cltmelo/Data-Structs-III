c8ae1a7ba7c63d1c0cfdf6b222456bd3ebac265b
JeanCassiano <115122121+JeanCassiano@users.noreply.github.com>
2023-12-16 11:53:04 -0300

    correção fun 10

--- trabalho_prático_2/13676461/graphs.c
+++ trabalho_prático_2/13676461/graphs.c
@@ -84,23 +84,33 @@ void imprime_Grafo(Grafo *gr, Lista *li){
     }
 }
 
+Grafo*  geraTransposto(Grafo* gr){
+    Grafo* trans = cria_Grafo(gr->nro_vertices, 20);
+    int i, j;
+    for(i=0; i < gr->nro_vertices; i++){
+        for(j=0; j < gr->grau_saida[i]; j++){
+            insereAresta(trans, gr->arestas[i][j], i, gr->pesos[i][j], gr->grupo[i]);
+        }
+    }
+    return trans;
 
+}
 void imprime_GrafoT(Grafo *gr, Lista *li){
     if(gr == NULL)
         return;
     int i, j;
     int* aux = indicesEmOrdemAlfabetica(li, tamanho_lista(li));
-
-    for(i=0; i < gr->nro_vertices - 1; i++){
+    Grafo *trans;
+    trans = geraTransposto(gr);
+    for(i=0; i < trans->nro_vertices - 1; i++){
         for (int k = 0; k < tamanho_lista(li); k++){
-            for(j=0; j < gr->grau_saida[aux[i] + 1]; j++){
-                if ((aux[k] + 1) == gr->arestas[aux[i] + 1][j]){
+            for(j=0; j < trans->grau_saida[aux[i] + 1]; j++){
+                if ((aux[k] + 1) == trans->arestas[aux[i] + 1][j]){
                     printTec(li, aux[i] + 1);
-                    printf(" ");
-                    printf("%d ", gr->grupo[aux[i] + 1]);
-                    printf("%d %d %d ", gr->grau_entrada[aux[i] + 1], gr->grau_saida[aux[i] + 1], gr->grau_saida[aux[i] + 1] + gr->grau_entrada[aux[i] + 1]);
-                    printTec(li, gr->arestas[aux[i] + 1][j]);
-                    printf(" %0.f", gr->pesos[aux[i] + 1][j]);
+                    printf(" %d ", gr->grupo[aux[i] + 1]);
+                    printf("%d %d %d ", trans->grau_entrada[aux[i] + 1], trans->grau_saida[aux[i] + 1], trans->grau_saida[aux[i] + 1] + trans->grau_entrada[aux[i] + 1]);
+                    printTec(li, trans->arestas[aux[i] + 1][j]);
+                    printf(" %0.f", trans->pesos[aux[i] + 1][j]);
                     printf("\n");
                 }
                 
@@ -111,6 +121,7 @@ void imprime_GrafoT(Grafo *gr, Lista *li){
 }
 
 
+
 void imprimirTecGerada(Grafo *gr, Lista *li, char* tec){
     if(gr == NULL)
         return;
@@ -118,18 +129,21 @@ void imprimirTecGerada(Grafo *gr, Lista *li, char* tec){
     int aux = busca_sequencial(li, tec);
     int *tecnologias;
     printf("%s: ", tec);
-    tecnologias = (int*) malloc (sizeof(int) * gr->grau_saida[aux]);
-    for(j=0; j < gr->grau_saida[aux]; j++){
-        tecnologias[j] = gr->arestas[aux][j];
+    Grafo *trans = geraTransposto(gr);
+    tecnologias = (int*) malloc (sizeof(int) * (trans->grau_saida[aux]));
+    for(j=0; j < trans->grau_saida[aux]; j++){
+        tecnologias[j] = trans->arestas[aux][j];
     }
+    i = 0;
+
     int contador = 0;
     int* tecs = indicesEmOrdemAlfabetica(li, tamanho_lista(li));
     for (i = 0; i < tamanho_lista(li); i++){
-        for (j = 0; j < gr->grau_saida[aux]; j++){
+        for (j = 0; j < trans->grau_saida[aux]; j++){
             if (tecs[i] + 1 == tecnologias[j]){
                 printTec(li, tecnologias[j]);
                 contador++;
-                if (contador < gr->grau_saida[aux]){
+                if (contador < trans->grau_saida[aux]){
                     printf(", ");
                 } else {
                     printf("\n\n");
@@ -197,7 +211,7 @@ Grafo*  geraGrafo(FILE* bin, Lista* lista){
 
     if(!lerCabecalho(bin, cabecalho)){
         printf("Registro Inexistente.\n");
-        return;
+        return NULL;
     }
     Grafo* gr = cria_Grafo(cabecalho->nroTecnologias + 1, 20);
     while (fread(&(registro->removido), sizeof(char), 1, bin) == 1) {
@@ -230,3 +244,132 @@ Grafo*  geraGrafo(FILE* bin, Lista* lista){
 
     return gr;
 }
+
+
+//=====================================================================================================================
+// TENATIVA DA FUNCIONALIDADE 11
+
+
+int dfs(Grafo* gr, int vert, int* visitado, int* pilha, int* tempo, int* componentes) {
+    visitado[vert] = 1;
+
+    for (int i = 0; i < gr->grau_saida[vert]; i++) {
+        int ind = gr->arestas[vert][i];
+        if (!visitado[ind]) {
+            *tempo += 1;
+            dfs(gr, ind, visitado, pilha, tempo, componentes);
+        }
+    }
+
+    pilha[*tempo] = vert;
+    *tempo += 1;
+    return 1;
+}
+
+Grafo* transporGrafo(Grafo* gr) {
+    Grafo* gr_transposto = cria_Grafo(gr->nro_vertices, gr->grau_max);
+
+    if (gr_transposto != NULL) {
+        for (int i = 0; i < gr->nro_vertices; i++) {
+            for (int j = 0; j < gr->grau_saida[i]; j++) {
+                int dest = gr->arestas[i][j];
+                float peso = gr->pesos[i][j];
+                insereAresta(gr_transposto, dest, i, peso, gr->grupo[i]);
+            }
+        }
+    }
+
+    return gr_transposto;
+}
+
+/* SO PARA TENTAR ENTENDER O QUE ESTÁ ACONTECENDO
+Grafo* cria_Grafo(int nro_vertices, int grau_max){
+    Grafo *gr;
+    gr = (Grafo*) malloc(sizeof(struct grafo));
+    if(gr != NULL){
+        int i;
+        gr->nro_vertices = nro_vertices;
+        gr->grau_max = grau_max;
+        gr->grau_entrada = (int*) calloc(nro_vertices,sizeof(int));
+        gr->grau_saida = (int*) calloc(nro_vertices,sizeof(int));
+        gr->grupo = (int*) calloc (nro_vertices, sizeof(int));
+        gr->arestas = (int**) malloc(nro_vertices * sizeof(int*));
+        for(i=0; i<nro_vertices; i++)
+            gr->arestas[i] = (int*) malloc(grau_max * sizeof(int));
+
+        gr->pesos = (float**) malloc(nro_vertices * sizeof(float*));
+        for(i=0; i<nro_vertices; i++)
+            gr->pesos[i] = (float*) malloc(grau_max * sizeof(float));
+        
+
+    }
+    return gr;
+}
+*/
+
+void dfsComponentes(Grafo* gr, int vert, int* visitado, int componente) {
+    visitado[vert] = 1;
+    gr->grupo[vert] = componente;
+
+    for (int i = 0; i < gr->grau_saida[vert]; i++) {
+        int ind = gr->arestas[vert][i];
+        if (!visitado[ind]) {
+            dfsComponentes(gr, ind, visitado, componente);
+        }
+    }
+}
+
+void componentesFortementeConexos(Grafo* gr) {
+    int* visitado = (int*)malloc(gr->nro_vertices * sizeof(int));
+    int* pilha = (int*)malloc(gr->nro_vertices * sizeof(int));
+    int tempo = 0;
+    int componente = 0;
+
+    // Inicializa o array de visitados
+    for (int i = 0; i < gr->nro_vertices; i++) {
+        visitado[i] = 0;
+    }
+
+    // Preenche a pilha com os vértices em ordem de término de DFS
+    for (int i = 0; i < gr->nro_vertices; i++) {
+        if (!visitado[i]) {
+            dfs(gr, i, visitado, pilha, &tempo, &componente);
+        }
+    }
+
+    // Transpõe o grafo
+    Grafo* gr_transposto = transporGrafo(gr);
+
+    // Reinicializa o array de visitados
+    for (int i = 0; i < gr->nro_vertices; i++) {
+        visitado[i] = 0;
+    }
+
+    // Processa os vértices em ordem da pilha para encontrar componentes fortemente conexos
+    for (int i = gr->nro_vertices - 1; i >= 0; i--) {
+        int vert = pilha[i];
+        if (!visitado[vert]) {
+            componente++;
+            dfsComponentes(gr_transposto, vert, visitado, componente);
+        }
+    }
+
+    // Conta o número de componentes fortemente conexos
+    int num_componentes = componente;
+
+    // Imprime o resultado
+    if (num_componentes == 1) {
+        printf("Sim, o grafo é fortemente conexo e possui 1 componente.\n");
+    } else {
+        printf("Nao, o grafo nao e fortemente conexo e possui %d componentes.\n", num_componentes);
+    }
+
+    // Libera a memória
+    free(visitado);
+    free(pilha);
+    libera_Grafo(gr_transposto);
+}
+
+
+//=====================================================================================================================
+// TENATIVA DA FUNCIONALIDADE 12
